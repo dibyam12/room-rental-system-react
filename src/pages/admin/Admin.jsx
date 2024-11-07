420c9f109c4f4426d73d296eb9f2abe559a1d6ad
chydeepak7 <deepakchy793@gmail.com>
2024-11-07 21:30:57 +0545

    admin doing

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,5 +1,28 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {fetchUserList} from "../../actions/userActions.jsx";
+
 const Admin = () => {
-  return <div>Admin</div>;
+  const dispatch = useDispatch();
+  const {users,loading,error} = useSelector(state => state.userList)
+  
+    {console.log(users, 'users')}
+  for (let [i,j] of Object.entries(users)) {
+    console.log(i,j,'sdfsf')
+  }
+
+  useEffect(()=> {
+  dispatch(fetchUserList())
+  }, [dispatch])
+  return (
+  <div>
+    test
+    {users.map(user => <div>{user.username}</div>)}
+    
+    
+   
+  
+  </div>)
 };
 
 export default Admin;
